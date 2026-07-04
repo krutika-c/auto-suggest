82d1489970a18e2c9d2239eeabfa9bc9ea2e6d3b
krutika <krutikachatrala@gmail.com>
2026-07-04 07:28:23 +0530

    added new features

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,20 @@
-console.log("Hello from js");
-alert("hello from js");
+const toggleBtn=document.getElementById("toggleBtn");
+const prof=document.getElementById("prof");
+const name=document.getElementById("name");
+const gender=document.getElementById("gender");
+
+let isJane=true;
+
+toggleBtn.addEventListener("click",()=>{
+    if (isJane) {
+        prof.src = "./images/john.png";
+        name.textContent = "John Doe";
+        gender.textContent = "Male";
+    } else {
+        prof.src = "./images/jane.png";
+        name.textContent = "Jane Doe";
+        gender.textContent = "Female";
+    }
+
+    isJane = !isJane;
+});
